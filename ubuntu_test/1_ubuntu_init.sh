e4e577c1fd7c5838f1da0acf4acc137ae7222dbb
ehejia <ehejia@qq.com>
2019-11-29 20:51:11 +0800

    添加旧版网络管理组件

--- ubuntu_test/1_ubuntu_init.sh
+++ ubuntu_test/1_ubuntu_init.sh
@@ -3,6 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 apt update 
 apt install -y git
+apt install -y net-tools
 apt install -y openssh-server
 service sshd start
 rm -rf ~/linux_setting
